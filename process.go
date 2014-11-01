9528bebc2fd126f40f3a66ddf21028de9803f63e
Al Tobey <tobert@gmail.com>
2014-10-31 17:26:14 -0700

    add license, go fmt

--- process.go
+++ process.go
@@ -1,5 +1,21 @@
 package sprok
 
+/*
+ * Copyright 2014 Albert P. Tobey <atobey@datastax.com> @AlTobey
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import (
 	"fmt"
 	"strings"
@@ -7,13 +23,11 @@ import (
 )
 
 type Process struct {
-	Env  map[string]string
-	Argv []string
-	// The file descriptors should be directly wired to these destinations. No attempt
-	// should be made to verify permissions or whatever. Fail fast.
-	Stdin  string
-	Stdout string
-	Stderr string
+	Env    map[string]string `"env"`
+	Argv   []string          `"argv"`
+	Stdin  string            `"stdin"`
+	Stdout string            `"stdout"`
+	Stderr string            `"stderr"`
 }
 
 // NewProcess returns a Process struct with the env map and argv allocated
